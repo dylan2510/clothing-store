aa01d3c4bedb69829cd4860c35836eb3386d103b
dylan2510 <dylan2510.dev@gmail.com>
2020-12-16 21:24:41 +0800

    update menu-items to navitage with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage.component';
 
+// HatsPage
+const HatsPage = () => {
+  return(
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <HomePage />
+    //<HomePage />
+
+    // Use routing
+    <switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </switch>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom'
 
 // {title} is actually props.title
-const MenuItem = ({title, imageUrl, size}) => {
+// history prop is accessible using withRouter()
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
     return(
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
             <div style={
                 {
                     backgroundImage: `url(${imageUrl})`
@@ -18,4 +20,5 @@ const MenuItem = ({title, imageUrl, size}) => {
     );
 };
 
-export default MenuItem;
+// withRouter() allow this component to access Router props such as history
+export default withRouter(MenuItem);
