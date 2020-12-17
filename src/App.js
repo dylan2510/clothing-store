580de41e141e40e2a73f33310cb3c0a02d5004ab
dylan2510 <dylan2510.dev@gmail.com>
2020-12-17 22:06:26 +0800

    added Header component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop.component';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-    //<HomePage />
-
-    // Use routing
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    // Header is outside router switchso it will always be rendered regardless of the url
+    <div>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
