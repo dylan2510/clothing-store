60d691b8175f4839cdafae342ecbbfbec4ac8823
dylan2510 <dylan2510.dev@gmail.com>
2020-12-26 14:52:01 +0800

    added cart-dropdown component and function

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
+import {connect} from "react-redux";
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItems}) => {
     return (
         <div className="cart-dropdown">
-            <div className="cart-items"></div>
+            <div className="cart-items">
+            {
+                cartItems.map(cartItem => {
+                    return (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    )
+                })
+            }
+            </div>
             <CustomButton>Go To Checkout</CustomButton>
         </div>
     );
 }
 
-export default CartDropdown;
+// this function take in the base reducer which is combineReducers() created in root-reducer.js
+// the reducer.user is pointing to the userReducer as defined in combineReducers()
+// the currentUser state is accessed via the userReducer
+
+const mapStateToProps = (reducer) => {
+    return {
+        cartItems: reducer.cart.cartItems
+    };
+  }
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,7 +9,7 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
         // if the item is the same item as item to add
         // return a new object for that item with all same properties, and quanity property + 1
         return cartItems.map(cartItem => cartItem.id === cartItemsToAdd.id ? 
-            {...cartItem, quanity: cartItem.quanity + 1}
+            {...cartItem, quantity: cartItem.quantity + 1}
             :
             cartItem
             )
@@ -17,6 +17,6 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
     // if it's a new item, return a new cart object 
     // with all current cart item and this new item with a quanity of 1
     else {
-        return [...cartItems, {...cartItemsToAdd, quanity: 1}]
+        return [...cartItems, {...cartItemsToAdd, quantity: 1}]
     }
 };
